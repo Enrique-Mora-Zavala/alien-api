2b8494b9afcd35764d8d83994471f10d94ec75ec
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-05 14:09:05 -0500

    Se empieza a implemengtar el CRUD y las rutas API

--- app/Http/Controllers/Api/AlienController.php
+++ app/Http/Controllers/Api/AlienController.php
@@ -3,47 +3,49 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Alien;
 use Illuminate\Http\Request;
 
 class AlienController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return Alien::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        return Alien::create([
+            'name' => $request->name,
+            'planet' => $request->planet,
+            'age' => $request->age,
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        return Alien::findOrFail($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $alien = Alien::findOrFail($id);
+
+        $alien->update([
+            'name' => $request->name,
+            'planet' => $request->planet,
+            'age' => $request->age,
+        ]);
+
+        return $alien;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        Alien::destroy($id);
+
+        return response()->json([
+            'message' => 'Alien eliminado'
+        ]);
     }
 }
