02ade1ba4d7d1a608b0d2b70093995fc33aa4560
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-05 14:04:25 -0500

    Se desarrolla el modelo Alien y se crea el controlador

--- app/Models/Alien.php
+++ app/Models/Alien.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Alien extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'planet',
+        'age'
+    ];
 }
